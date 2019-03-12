d81800b989b3576b12b4eecc0786e447a0ef41b4
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-03-12 21:45:13 +0100

    chore: setup formatting, linting and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'json'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'no-fallthrough': 'error',
+  },
+};
