4542f524e92526c22aacfa35c1b18d93bfec29a3
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-05-05 20:40:12 +0200

    feat: add configuration

---
+++ src/config/config.ts
@@ -0,0 +1,33 @@
+import * as convict from 'convict';
+import 'dotenv/config';
+
+const config = convict({
+  aws: {
+    accessKeyId: {
+      format: String,
+      env: 'AWS_ACCESS_KEY_ID',
+      sensitive: true,
+      default: '',
+    },
+    secretAccessKey: {
+      format: String,
+      env: 'AWS_SECRET_ACCESS_KEY',
+      sensitive: true,
+      default: '',
+    },
+    region: {
+      format: String,
+      env: 'AWS_REGION',
+      default: 'eu-central-1',
+    },
+    bucketName: {
+      format: String,
+      env: 'AWS_BUCKET_NAME',
+      default: 'homebrew-pc',
+    },
+  },
+});
+
+config.validate({ allowed: 'strict' });
+
+export default config;
